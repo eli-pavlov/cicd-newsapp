24822e429427ddd0e6e3e496be49d65d77e0e5ac
Eli Pavlov <pavlov.eli@gmail.com>
2025-08-14 20:54:41 +0300

    update files

--- frontend/nginx/entrypoint.sh
+++ frontend/nginx/entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 set -eu
-
-# Defaults (work in dev out-of-the-box)
 : "${BACKEND_SERVICE_HOST:=backend.backend.svc.cluster.local}"
 : "${BACKEND_SERVICE_PORT:=8080}"
 
-# Render nginx config from template
 envsubst '$BACKEND_SERVICE_HOST $BACKEND_SERVICE_PORT' \
   < /etc/nginx/templates/default.conf.template \
   > /etc/nginx/conf.d/default.conf
