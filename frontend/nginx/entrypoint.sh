98ebaa0a4a4ecb97118bf876bd805e97952066db
Eli Pavlov <pavlov.eli@gmail.com>
2025-08-14 22:01:56 +0300

    update repo

--- frontend/nginx/entrypoint.sh
+++ frontend/nginx/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -eu
-: "${BACKEND_SERVICE_HOST:=backend.backend.svc.cluster.local}"
+
+NS="${POD_NAMESPACE:-default}"
+: "${BACKEND_SERVICE_HOST:=backend.${NS}.svc.cluster.local}"
 : "${BACKEND_SERVICE_PORT:=8080}"
 
 envsubst '$BACKEND_SERVICE_HOST $BACKEND_SERVICE_PORT' \
