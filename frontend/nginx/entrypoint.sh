b8955abd68e629e7e8190379cc8727679c56a571
eli-pavlov <pavlov.eli@gmail.com>
2025-08-18 13:01:20 +0300

    update

--- frontend/nginx/entrypoint.sh
+++ frontend/nginx/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 set -eu
-# no envsubst needed anymore
+
+NS="${POD_NAMESPACE:-default}"
+: "${BACKEND_SERVICE_HOST:=backend.${NS}.svc.cluster.local}"
+: "${BACKEND_SERVICE_PORT:=8080}"
+
+envsubst '$BACKEND_SERVICE_HOST $BACKEND_SERVICE_PORT' \
+  < /etc/nginx/templates/default.conf.template \
+  > /etc/nginx/conf.d/default.conf
+
 exec "$@"
